cddb5d3d71f5c02bd808dfbeefd6f5b1c8137a10
Steve Russell <stephen.russell@lexisnexisrisk.com>
2021-10-20 20:08:38 -0700

    INFRA-389 Address linter complaints

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe 'irqbalance' do
-
   context 'without parameters' do
     let(:facts) do
       {
@@ -9,60 +8,58 @@ describe 'irqbalance' do
       }
     end
 
-    it { should compile.with_all_deps }
+    it { is_expected.to compile.with_all_deps }
     it {
-      is_expected.to contain_package('irqbalance').
-        with_ensure('installed').
-        that_notifies('Service[irqbalance.service]')
+      is_expected.to contain_package('irqbalance')
+        .with_ensure('installed')
+        .that_notifies('Service[irqbalance.service]')
     }
 
     it {
-      is_expected.to contain_service('irqbalance.service').
-        with_ensure('running').
-        with_enable(true)
+      is_expected.to contain_service('irqbalance.service')
+        .with_ensure('running')
+        .with_enable(true)
     }
 
     it {
-      is_expected.to contain_file('/usr/lib/systemd/system/irqbalance.service').
-        with_ensure('file').
-        with_owner('root').
-        with_group('root').
-        with_mode('0644')
+      is_expected.to contain_file('/usr/lib/systemd/system/irqbalance.service')
+        .with_ensure('file')
+        .with_owner('root')
+        .with_group('root')
+        .with_mode('0644')
     }
 
     it {
-      is_expected.to contain_file('/usr/lib/systemd/system/irqbalance.service.d').
-        with_ensure('directory').
-        with_owner('root').
-        with_group('root').
-        with_mode('0755')
+      is_expected.to contain_file('/usr/lib/systemd/system/irqbalance.service.d')
+        .with_ensure('directory')
+        .with_owner('root')
+        .with_group('root')
+        .with_mode('0755')
     }
 
     it {
-      is_expected.to contain_file('/usr/lib/systemd/system/irqbalance.service.d/puppet.conf').
-        with_ensure('file').
-        with_owner('root').
-        with_group('root').
-        with_mode('0644').
-        that_notifies('Service[irqbalance.service]').
-        that_notifies('Class[systemd::systemctl::daemon_reload]').
-        with_content(/\[Service\]/).
-        with_content(/Type=simple/).
-        with_content(/EnvironmentFile=\/etc\/sysconfig\/irqbalance/).
-        with_content(/ExecStart=\/usr\/sbin\/irqbalance --foreground \$IRQBALANCE_ARGS/)
+      is_expected.to contain_file('/usr/lib/systemd/system/irqbalance.service.d/puppet.conf')
+        .with_ensure('file')
+        .with_owner('root')
+        .with_group('root')
+        .with_mode('0644')
+        .that_notifies('Service[irqbalance.service]')
+        .that_notifies('Class[systemd::systemctl::daemon_reload]')
+        .with_content(%r{\[Service\]})
+        .with_content(%r{Type=simple})
+        .with_content(%r{EnvironmentFile=\/etc\/sysconfig\/irqbalance})
+        .with_content(%r{ExecStart=\/usr\/sbin\/irqbalance --foreground \$IRQBALANCE_ARGS})
     }
 
     it {
-      is_expected.to contain_file('/etc/sysconfig/irqbalance').
-        with_ensure('file').
-        with_owner('root').
-        with_group('root').
-        with_mode('0644').
-        that_notifies('Service[irqbalance.service]').
-        with_content(/#IRQBALANCE_ONESHOT="no"/).
-        with_content(/#IRQBALANCE_BANNED_CPUS=""/)
+      is_expected.to contain_file('/etc/sysconfig/irqbalance')
+        .with_ensure('file')
+        .with_owner('root')
+        .with_group('root')
+        .with_mode('0644')
+        .that_notifies('Service[irqbalance.service]')
+        .with_content(%r{#IRQBALANCE_ONESHOT="no"})
+        .with_content(%r{#IRQBALANCE_BANNED_CPUS=""})
     }
-
   end
-
 end
